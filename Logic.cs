a1e7e5a94f86cd6d48b9792fc59f31f56c28a9d6
Dmitry Daschinsky <ddaschinsky@gmail.ru>
2013-06-23 18:01:28 +0300

    Creation project on github

---
+++ Logic.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Checkers
+{
+    public class Logic
+    {
+        public Move GetMove(Board tmpBoard)
+        {
+            Move currentMove = new Move();
+            for (int x = 1; x < 9; x++)
+            {
+                for (int y = 1; y < 9; y++)
+                {
+
+                }
+            }
+
+
+
+
+                return currentMove;
+        }
+    }
+}
